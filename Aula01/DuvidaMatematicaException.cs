24fdcf3b86ca1c1d804d0e7219d05a67db5af6fe
Aula <Aula@CANP-LAB7-C01>
2025-05-09 10:39:55 -0300

    Adicionar arquivos de projeto.

---
+++ Aula01/DuvidaMatematicaException.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+[Serializable]
+internal class DuvidaMatematicaException : Exception
+{
+    public DuvidaMatematicaException()
+    {
+    }
+
+    public DuvidaMatematicaException(string? message) : base(message)
+    {
+    }
+
+    public DuvidaMatematicaException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
---
+++ Aula01/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Informe seu nome: ");
+string nome = Console.ReadLine();
+Console.WriteLine($"Seja bem-vindo(a) {nome}");
+Console.WriteLine("Informe sua idade: ");
+try
+{
+    int idade = int.Parse(Console.ReadLine());
+    if ( idade == 1)
+    {
+        throw new DuvidaMatematicaException();
+    }
+    Console.WriteLine("A idade informada foi " + idade);
+    if (idade % 2 == 0)
+    {
+        Console.WriteLine("A idade informada corresponde a um número par!");
+    }
+    else
+    {
+        Console.WriteLine("A idade informada corresponde a um número ímpar!");
+    }
+    string primo = "";
+    for (int i = 2; i <= idade / 2; i++)
+    {
+        if (idade % i == 0)
+        {
+            primo = "não ";
+            break;
+        }
+    }
+    Console.WriteLine($"A idade {primo} corresponde a um número primo");
+}
+catch(FormatException e)
+{
+    Console.WriteLine("A idade digitada não corresponde a um número inteiro");
+}
+
+catch(DuvidaMatematicaException e)
+{
+    Console.WriteLine("Não se sabe se um é primo ou não!");
+}
+catch(Exception e)
+{
+    Console.WriteLine("Ocorreu um erro desconhecido, contate o desenvolvedor.");
+}
+
